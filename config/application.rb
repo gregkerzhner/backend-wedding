780d066472668abac475b2ae8e75db4905455b06
Greg Kerzhner <guidoprincess@gmail.com>
2015-10-05 12:45:42 -0700

    Disabling assets

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module BackendWedding
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.enabled = false
+    config.assets.precompile = []
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
